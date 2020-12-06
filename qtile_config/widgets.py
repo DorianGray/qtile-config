c12b92eebf3f351c4828359924b9eb12c418fe01
Drew Ditthardt <drew.ditthardt@gmail.com>
2020-12-06 12:55:04 -0500

    import cleanup

--- qtile_config/widgets.py
+++ qtile_config/widgets.py
@@ -1,10 +1,10 @@
+import libqtile
 from .widget import (
     Power,
     Bar,
     QTile,
 )
 from . import theme
-from libqtile import widget
 
 
 __all__ = [
@@ -21,12 +21,12 @@ __all__ = [
 
 
 qtile = QTile()
-group_box = widget.GroupBox()
-prompt = widget.Prompt()
-task_list = widget.TaskList()
-spacer = widget.Spacer()
-systray = widget.Systray()
-clock = widget.Clock(**theme.clock)
+group_box = libqtile.widget.GroupBox()
+prompt = libqtile.widget.Prompt()
+task_list = libqtile.widget.TaskList()
+spacer = libqtile.widget.Spacer()
+systray = libqtile.widget.Systray()
+clock = libqtile.widget.Clock(**theme.clock)
 power = Power(**theme.power)
 bar = Bar(
     [
